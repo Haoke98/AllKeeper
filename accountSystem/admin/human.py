e38296af0d29128a83192a0ea9d20fed5c0b225f
Sadam·Sadik <1903249375@qq.com>
2023-06-13 18:52:37 +0800

    升级了社工库的手机号关联性表单和关联展示列表

--- accountSystem/admin/human.py
+++ accountSystem/admin/human.py
@@ -7,19 +7,62 @@
 @disc:
 ======================================="""
 from django.contrib import admin
+from django.forms import ModelForm
+from django.utils.safestring import mark_safe
 
 from izBasar.admin import LIST_DISPLAY
 from ..models import Human, Account
+from ..models import Tel
+
+
+class TelForm(ModelForm):
+    class Meta:
+        model = Tel
+        fields = ['content', 'remark']
+
+
+class TelInlineAdmin(admin.TabularInline):
+    model = Tel
+    form = TelForm
+    min_num = 0
+    extra = 0
 
 
 @admin.register(Human)
 class HumanAdmin(admin.ModelAdmin):
-    list_display = LIST_DISPLAY + ["name", 'idCardNum', 'sex', '_count', 'info']
+    list_display = LIST_DISPLAY + ["name", 'idCardNum', 'sex', '_tels', '_count', 'info']
     search_fields = ['name', 'idCardNum']
     list_filter = ['sex']
     list_per_page = 14
+    inlines = [TelInlineAdmin]
 
     def _count(self, obj):
         return Account.objects.filter(types=obj.id).count()
 
     _count.short_description = "所关联的账号数量"
+
+    def _getTelItem(self, tel: Tel):
+        item = '''
+                        <div class="item">
+                            <i class="phone square icon"></i>
+                            <div class="content">
+                              <a class="header">%s</a>
+
+                            </div>
+                        </div>
+                ''' % tel.content
+        return mark_safe(item)
+
+    def _tels(self, obj):
+        items: str = ""
+        tels = Tel.objects.filter(owner=obj).all()
+        for tel in tels:
+            items += self._getTelItem(tel)
+        finalList = '''
+                <div class="ui list">
+                    %s              
+                </div>
+        ''' % items
+        return mark_safe(finalList)
+
+    _tels.short_description = "联系方式"
--- accountSystem/admin/tel.py
+++ accountSystem/admin/tel.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from izBasar.admin import LIST_DISPLAY
 from ..models import Tel
 
