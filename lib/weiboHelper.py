bc54d45c712d9350f859ca6a53f55960724b6310
Sadam·Sadik <1903249375@qq.com>
2023-07-10 15:34:52 +0800

    1.解决了Emoji符号无法保存异常,2.初步实现了微博账户模型的设计和实现并Admin页面的设计.

--- lib/weiboHelper.py
+++ lib/weiboHelper.py
@@ -1,16 +1,15 @@
 # _*_ codign:utf8 _*_
 """====================================
 @Author:Sadam·Sadik
-@Email：1903249375@qq.com
-@Date：2023/7/6
+@Email:1903249375@qq.com
+@Date:2023/7/6
 @Software: PyCharm
 @disc:
 ======================================="""
 import requests
 
 
-class Weibo:
-    def test(wb_id: str):
+def test(wb_id: str):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
@@ -18,8 +17,17 @@ class Weibo:
         with open(f"{wb_id}.html", "wb") as f:
             f.write(resp.content)
 
-    @staticmethod
-    def info(uid: str):
+
+def info(uid: str):
+        """
+        :return 
+        {'ok': 1, 
+        'data': 
+                {
+                    'user': {'id': 7064711748, 'idstr': '7064711748', 'pc_new': 0, 'screen_name': 'Sardarior', 'profile_image_url': 'https://tvax1.sinaimg.cn/crop.0.0.512.512.50/007I6OfGly8h896ioim87j30e80e8aae.jpg?KID=imgbed,tva&Expires=1688976518&ssig=0zay%2BiA488', 'profile_url': '/u/7064711748', 'verified': False, 'verified_type': -1, 'domain': '', 'weihao': '', 'avatar_large': 'https://tvax1.sinaimg.cn/crop.0.0.512.512.180/007I6OfGly8h896ioim87j30e80e8aae.jpg?KID=imgbed,tva&Expires=1688976518&ssig=8W5YwfQZvX', 'avatar_hd': 'https://tvax1.sinaimg.cn/crop.0.0.512.512.1024/007I6OfGly8h896ioim87j30e80e8aae.jpg?KID=imgbed,tva&Expires=1688976518&ssig=z2UP47JX%2B3', 'follow_me': False, 'following': False, 
+                    'mbrank': 3, 'mbtype': 2, 'v_plus': 0, 'planet_video': False, 'description': '喜欢唱歌', 'location': '北京', 'gender': 'm', 'followers_count': 237, 'followers_count_str': '237', 'friends_count': 225, 'statuses_count': 38, 'url': '', 'svip': 0, 'cover_image_phone': 'https://ww1.sinaimg.cn/crop.0.0.640.640.640/9d44112bjw1f1xl1c10tuj20hs0hs0tw.jpg', 'icon_list': [], 'user_type': 2, 'is_star': '0', 'is_muteuser': False, 'special_follow': False},
+        'tabList': [{'name': 'home', 'tabName': '精选'}, {'name': 'feed', 'tabName': '微博'}, {'name': 'newVideo', 'tabName': '视频'}, {'name': 'album', 'tabName': '相册'}], 'blockText': ''}}
+        """
         resp = requests.get(f"https://weibo.com/ajax/profile/info?uid={uid}", headers={
             "Cookie": "SUB=_2AkMU0XJPf8NxqwJRmfoRxG7gboVyyw3EieKijYOUJRMxHRl-yT9kqkI9tRB6P1FcoA_jiiwlWkU1njUraklGH8yyp3eg; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9W5o-1D0KVWj47oMwKycwlRH; SINAGLOBAL=3835450204212.678.1670249850113; ULV=1670249850168:1:1:1:3835450204212.678.1670249850113:; XSRF-TOKEN=szpHdnejDXDZYbx6Z4qSgioL; WBPSESS=1QIptkPh0r7VTljIOfRP67LOhyoJ1yOf94TLlawIatqfNeS0xdW8Sei-I-A4Dksn0mlpb1M64xnYWdD0Pas_puBut1fMAOtq8YLZdv81Xf8Dc5AwXo-3S4K8jrf0VE5Rcj3F2sMwDYgSb4nxONnzki84VKkpLKAh5pPuJTTE-WI="
         })
@@ -32,8 +40,8 @@ class Weibo:
         # img = Image.open(io.BytesIO(image))
         # img.show()
 
-    @staticmethod
-    def detail(uid: str):
+    
+def detail(uid: str):
         """
         :param uid: 微博里的用户系统ID
         :return:{'data': {'followers': {'total_number': 0}, 'education': {'school': '首都医科大学丰台校区'}, 'birthday': '2000-08-26 处女座', 'created_at': '2019-04-04 16:00:06', 'description': '喜欢唱歌', 'gender': 'm', 'location': '北京', 'ip_location': '', 'sunshine_credit': {'level': '信用较好'}, 'label_desc': [{'name': '视频累计播放量3.9万', 'normal_mode': {'word_color': '#FFFF6200', 'background_color': '#59FFDDCB'}, 'dark_mode': {'word_color': '#FFEA8011', 'background_color': '#FF181818'}, 'scheme_url': ''}], 'desc_text': '', 'verified_url': 'https://verified.weibo.com/verify?fr=myverified?uid=7064711748'}, 'ok': 1}
@@ -46,4 +54,5 @@ class Weibo:
 
 
 if __name__ == '__main__':
-    Weibo.detail("7064711748")
+    # detail("7064711748")
+    info("7064711748")
