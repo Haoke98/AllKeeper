88b349735cc2a08239357e18f7beb94553b642b7
Sadam·Sadik <1903249375@qq.com>
2023-07-13 11:44:07 +0800

    服务器admin列表中的列宽和其他参数进行了小调整

--- accountSystem/admin/server.py
+++ accountSystem/admin/server.py
@@ -1,27 +1,29 @@
 from django.contrib import admin
 
 from izBasar.admin import BaseAdmin
-from .serverUser import ServerUserInlineAdmin
 from ..forms import ServerForm
 from ..models import Server
 
 
 @admin.register(Server)
 class ServerAdmin(BaseAdmin):
-    list_display = ["id", "_ip", '_password', 'ssh', '_biosPassword', 'remark', 'group', 'hoster', "createdAt",
+    list_display = ["id", "_ip", '_password', 'ssh', '_biosPassword',  'group', 'hoster','remark', "createdAt",
                     "updatedAt",
                     "deletedAt", ]
     list_display_links = ['id', 'remark', 'group', 'hoster']
     list_filter = ['group', 'hoster', 'ssh']
     date_hierarchy = 'updatedAt'
-    search_fields = ['ip', 'remark', 'hoster']
+    search_fields = ['ip', 'remark']
     search_help_text = ['你好，这是搜索帮助语句！']
     autocomplete_fields = ['group']
     form = ServerForm
-    #inlines = [ServerUserInlineAdmin]
+
+    # inlines = [ServerUserInlineAdmin]
 
     def _ip(self, obj):
         return BaseAdmin.username(obj.ip)
+
+    _ip.short_description = "IP地址"
 
     def _password(self, obj):
         return BaseAdmin.password(obj.rootPassword)
@@ -33,7 +35,6 @@ class ServerAdmin(BaseAdmin):
 
     _biosPassword.short_description = "BIOS密码"
 
-
     class Media:
 
         def __init__(self):
@@ -44,3 +45,47 @@ class ServerAdmin(BaseAdmin):
         js = [
         ]
 
+    fields_options = {
+        'id': {
+            'fixed': 'left',
+            'width': '40px',
+            'align': 'center'
+        },
+        'createdAt': {
+            'width': '180px',
+            'align': 'left'
+        },
+        'updatedAt': {
+            'width': '180px',
+            'align': 'left'
+        },
+        '_ip': {
+            'width': '140px',
+            'align': 'center'
+        },
+        'ssh': {
+            'width': '120px',
+            'align': 'center'
+        },
+        'hoster': {
+            'width': '320px',
+            'align': 'left'
+        },
+        'group': {
+            'width': '220px',
+            'align': 'left'
+        },
+        '_password': {
+            'width': '80px',
+            'align': 'center'
+        },
+        'remark': {
+            'width': '200px',
+            'align': 'left'
+        },
+
+        '_biosPassword': {
+            'width': '100',
+            'align': 'center'
+        }
+    }
