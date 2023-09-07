8792468318d622a996655483e5216241994f3329
Sadam·Sadik <1903249375@qq.com>
2023-09-07 17:02:32 +0800

    优化升级了异常Traceback的记录和展示

--- icloud/admin.py
+++ icloud/admin.py
@@ -34,7 +34,8 @@ STATUS = STATUS_STOP
 PROGRESS = -1
 TOTAL = -1
 STARTED_AT = datetime.datetime.now()
-EXCEPTION_TRACE = None
+EXCEPTION_MSG = None
+EXCEPTION_TRACE_BACK = None
 
 
 def download_thumb(obj: IMedia, p):
@@ -116,7 +117,7 @@ def collect(p, album, i, total):
 
 
 def collect_all_medias():
-    global STATUS, PROGRESS, TOTAL, STARTED_AT, EXCEPTION_TRACE
+    global STATUS, PROGRESS, TOTAL, STARTED_AT, EXCEPTION_MSG,EXCEPTION_TRACE_BACK
     # for album in albums:
     #     photos = iService.photos.albums[album.name]
     #     total = len(photos)
@@ -143,7 +144,8 @@ def collect_all_medias():
         STATUS = STATUS_FINISHED
     except Exception as e:
         STATUS = STATUS_EXCEPTION
-        EXCEPTION_TRACE = str(e) + "\n" + traceback.format_exc()
+        EXCEPTION_MSG = str(e)
+        EXCEPTION_TRACE_BACK = traceback.format_exc()
         print(traceback.format_exc())
 
 
@@ -289,9 +291,18 @@ class IMediaAdmin(admin.ModelAdmin):
         dlt1 = datetime.timedelta(seconds=dlt_in_second)
         willFinishedAt = datetime.datetime.now() + dlt1
         return f'''
-        <el-alert title="状态：{STATUS}, 进度: {PROGRESS:.2f}% ({finishedCount}/{TOTAL}) 开始于：{STARTED_AT}, 运行了:{dlt}, 速率：{speed_in_second}, 剩余：{left}, 还需要：{dlt1}, 即将完成于：{willFinishedAt}, 异常信息:{EXCEPTION_TRACE}" type="{_type}">
+        <el-alert title="状态：{STATUS}, 进度: {PROGRESS:.2f}% ({finishedCount}/{TOTAL}) 开始于：{STARTED_AT}, 运行了:{dlt}, 速率：{speed_in_second}, 剩余：{left}, 还需要：{dlt1}, 即将完成于：{willFinishedAt}" type="{_type}">
             <el-progress type="circle" percentage="{PROGRESS:.2f}" color="#5cb87a"></el-progress>
-        </el-alert>'''
+        </el-alert>
+        <el-collapse accordion>
+          <el-collapse-item>
+            <template slot="title">
+              异常： {EXCEPTION_MSG}<i class="header-icon el-icon-info"></i>
+            </template>
+            <pre>{EXCEPTION_TRACE_BACK}</pre>
+          </el-collapse-item>
+        </el-collapse>
+        '''
 
     def has_add_permission(self, request):
         return False
