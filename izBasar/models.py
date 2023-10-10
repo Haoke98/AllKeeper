3ccc00930cd51f2707cbc7e9db4c42bd978be113
Sadam·Sadik <1903249375@qq.com>
2023-10-10 17:30:21 +0800

    升级了BaseModel中的info字段用simplepro中的UE编辑器字段重构了

--- izBasar/models.py
+++ izBasar/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from simplepro.editor import fields
 
 
 class BaseModel(models.Model):
@@ -7,7 +8,7 @@ class BaseModel(models.Model):
                                      editable=False, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True, verbose_name="最近更新时间", null=True, editable=False, db_index=True)
     deletedAt = models.DateTimeField(verbose_name="被删除时间", null=True, editable=False, db_index=True)
-    info = models.TextField(verbose_name="说明", null=True, blank=True)
+    info = fields.UETextField(verbose_name='说明', null=True, blank=True)
 
     class Meta:
         abstract = True
