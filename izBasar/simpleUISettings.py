2307489f9d65f1fc475dfd1fc45a87236c933820
Sadam·Sadik <1903249375@qq.com>
2023-11-20 15:54:08 +0800

    初步引入并设置了自定义菜单配置

--- izBasar/simpleUISettings.py
+++ izBasar/simpleUISettings.py
@@ -1,3 +1,4 @@
+from . import _STATIC_URL
 # SIMPLE UI config.
 SIMPLEUI_DEFAULT_THEME = 'x-green.css'
 
@@ -27,9 +28,125 @@ SIMPLEUI_ICON = {
     '所有服务器用户': 'fas fa-user-astronaut',
     'Elastic searchs': 'el-icon-s-data',
     'House price types': 'fas fa-file-invoice-dollar',
-    'Phone numbers': 'fas fa-phone-square'
+    'Phone numbers': 'fas fa-phone-square',
+    '万能堡垒': 'fab fa-fort-awesome',
+    'SSH服务': 'fas fa-terminal',
+    'SSH账号': 'fas fa-users'
 }
-from . import _STATIC_URL
+SIMPLEUI_CONFIG = {
+    # 在自定义菜单的基础上保留系统模块
+    'system_keep': True,
+    'dynamic': False,
+    'menus': [
+        {
+            'name': '万能堡垒',
+            'icon': 'fab fa-fort-awesome',
+            'codename': 'jump-server',
+            'models': [
+                {
+                    'name': '设备管理',
+                    'codename': 'devices-management',
+                    'icon': 'fas fa-server',
+                    'models': [
+                        {
+                            'name': '服务管理',
+                            'icon': 'fas fa-server',
+                            'url': 'https://haoke98.github.io/DjangoAsyncAdmin/'
+                        },
+                        {
+                            'name': '路由器管理',
+                            'icon': 'fas fa-wifi',
+                            'url': 'https://github.com/Haoke98/DjangoAsyncAdmin'
+                        },
+                        {
+                            'name': '交换机管理',
+                            'icon': 'fas fa-random',
+                            'url': 'https://github.com/Haoke98/DjangoAsyncAdmin'
+                        }
+
+                    ]
+                }, {
+                    'name': '服务管理',
+                    'url': 'https://github.com/newpanjing/simpleui',
+                    'icon': 'fas fa-poll-h',
+                    'codename': 'service-management',
+                    'models': [
+                        {
+                            'name': 'SSH服务',
+                            'icon': "fas fa-terminal",
+                            'url': 'https://haoke98.github.io/DjangoAsyncAdmin/'
+                        }, {
+                            'name': '数据服务',
+                            'url': 'https://github.com/Haoke98/DjangoAsyncAdmin'
+                        }
+                    ]
+                }, {
+                    'name': '网段管理',
+                    'url': 'https://convert.72wo.com',
+                    'icon': 'fas fa-network-wired',
+                    'codename': 'convert'
+                }, {
+                    'name': '穿透管理',
+                    'url': 'https://github.com/sea-team/gofound',
+                    'icon': 'fab fa-hive',
+                    'codename': 'gofound'
+                }
+            ]
+        },
+        {
+            'name': 'GitHub',
+            'icon': 'fas fa-code',
+            'url': 'https://haoke98.github.io/AllKeeper/',
+            'codename': 'community'
+        },
+        {
+            'name': 'Bug反馈',
+            'icon': 'fas fa-bug',
+            'url': 'https://github.com/Haoke98/AllKeeper/issues',
+            'codename': 'bug_trace',
+            'newTab': True
+        },
+        {
+            'name': '依赖',
+            'icon': 'fas fa-project-diagram',
+            'codename': 'product',
+            'models': [
+                {
+                    'name': 'DjangoAsyncAdmin',
+                    'codename': 'django_async_admin',
+                    'icon': 'fas fa-bug',
+                    'models': [
+                        {
+                            'name': '文档',
+                            'url': 'https://haoke98.github.io/DjangoAsyncAdmin/'
+                        }, {
+                            'name': 'Github',
+                            'url': 'https://github.com/Haoke98/DjangoAsyncAdmin'
+                        }
+                    ]
+                }, {
+                    'name': '图标',
+                    'url': 'https://fontawesome.com/',  # TODO：后期把这个改成一个单独的页面，里面渲染出所有能够调用的图标，外加图标检索功能
+                    'icon': 'fas fa-icons',
+                    'codename': 'icon',
+                    'newTab': True
+                }, {
+                    'name': '图片转换器',
+                    'url': 'https://convert.72wo.com',
+                    'icon': 'fab fa-github',
+                    'codename': 'convert',
+                    'newTab': True
+                }, {
+                    'name': '全文检索',
+                    'url': 'https://github.com/sea-team/gofound',
+                    'icon': 'fab fa-github',
+                    'codename': 'gofound',
+                    'newTab': True
+                }
+            ]
+        }
+    ]
+}
 
 SIMPLEUI_LOGO = _STATIC_URL + 'img/LOGO.png'
 SIMPLEUI_HOME_INFO = False  # 首页上的simpleUI的版本信息板块。
