7cef407a8e0873a035a247f7048daf6a983cb2a0
Sadam·Sadik <1903249375@qq.com>
2023-12-08 18:00:00 +0800

    重新调整了jumpServer的模型目录结构，便于后期妥善管理

---
+++ jumpService/models/services/__init__.py
@@ -0,0 +1,14 @@
+# _*_ codign:utf8 _*_
+"""====================================
+@Author:Sadam·Sadik
+@Email：1903249375@qq.com
+@Date：2023/12/8
+@Software: PyCharm
+@disc:
+======================================="""
+from .bt import BT
+from .db import DbService, DbServiceUser
+from .es import ElasticSearch
+from .minio import MinIO
+from .service import Service, ServiceUser
+from .ssh import SSHServiceUser, SSHService
